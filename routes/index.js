012ef5c58fd42f4f928ee965b7fed8ffe96d1e2b
rhlgr1 <rg512g1@gmail.com>
2022-12-27 15:18:43 +0530

    changes done

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const User = require("../model/userModel");
-const passport = require("passport");
-const localStr = require("passport-local");
 
-passport.use(new localStr(User.authenticate()))
+const User = require('../model/userModel');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+passport.use(new localStrategy(User.authenticate()));
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,81 +15,105 @@ router.get('/', function (req, res, next) {
 router.get('/signup', function (req, res, next) {
   res.render('signup');
 });
-
 router.get('/signin', function (req, res, next) {
   res.render('signin');
 });
-
-router.post('/signup', function (req, res, next) {
-
-  const newuser = {
-    username: req.body.username,
-    email: req.body.email,
-    name: req.body.name,
-    phone: req.body.phone,
-  }
-  User.register(newuser, req.body.password)
-    .then((registeruser) => {
-      res.redirect('/signin');
-    })
-    .catch((err) => res.send(err))``
-});
-
-router.post('/signin', passport.authenticate("local", {
-  successRedirect: "/profile",
-  failureRedirect: "/"
-}), function (req, res, next) {
-
-  res.redirect('/profile');
-
-});
-
-
-router.get('/profile', isLoggedIn, function (req, res, next) {
-  res.render("profile", { user: req.session.passport.user })
-});
-
-router.get('/logout', isLoggedIn, function (req, res, next) {
-  req.logout(function () {
-    res.redirect("/");
-  });
-});
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  res.redirect("/")
-}
 
 router.get('/forget', function (req, res, next) {
   res.render('forget');
 });
 
-router.post('/forget', function (req, res, next) {
-  User.findOne({ username: req.body.username })
-    .then((userfound) => {
-      if (userfound === null)
-        return res.send("not found <a href='/'>home</a>")
-      userfound.setPassword(req.body.password, function (err, user) {
-        userfound.save();
-        res.redirect("/")
-      })
+router.get('/reset', function (req, res, next) {
+  res.render('reset');
+});
+
+
+router.post('/signup', function (req, res, next) {
+  const { username, name, phone, password, email } = req.body;
+  const newUser = {
+    username, email, phone, name
+  }
+  User.register(newUser, password)
+    .then((u) => {
+      res.redirect('/signin');
     })
-    .catch((err) => res.send(err));
+    .catch((err) => {
+      res.send(err);
+    });
+
 });
 
-router.post('/reset', function (req, res, next) {
-  const { oldpassword, password } = req.body;
-  req.user.changePassword(oldpassword, password, function (err, user) {
-    res.redirect("/")
-  })
+router.post('/signin', passport.authenticate('local', {
+  successRedirect: "/profile",
+  failureRedirect: "/",
+}),
+  function (req, res, next) { }
+);
+
+
+router.post("/forget", function (req, res, next) {
+  User.findOne({ username: req.body.username })
+      .then((userFound) => {
+          if (userFound === null)
+              return res.send("not found <a href='/'>home</a>");
+
+          userFound.setPassword(req.body.password, function (err, user) {
+              userFound.save();
+              res.redirect("/");
+          });
+      })
+      .catch((err) => res.send(err));
 });
 
 
 
+router.get('/profile', isLoggedIn, function (req, res) {
+  res.render("profile", { user: req.session.passport.user });
+  // console.log(req.session);
+  // console.log(req.session.passport);
+});
 
 
+router.post("/change", isLoggedIn, function (req, res, next) {
+  // res.send(req.user);
+
+  const { oldpassword, newpassword } = req.body;
+  req.user.changePassword(oldpassword, newpassword, function (err, user) {
+      if (err) {
+          res.send(err);
+      }
+      res.redirect("/logout");
+  });
+});
+
+// router.post("/reset", function (req, res, next) {
+//   User.find({ username: req.body.username, oldpassword: req.body.oldpassword })
+//   .then((userFound) => {
+//       if (userFound === null)
+//           return res.send("not found <a href='/'>home</a>");
+//   userFound.changePassword(oldpassword, newpassword, function (err, user) {
+//     userFound.save();
+//       res.redirect("/");
+//     })
+//     .catch((err) => res.send(err));
+// });
+// });
+
+router.get("/logout", isLoggedIn, function (req, res, next) {
+  req.logout(function () {
+      res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      next();
+      return;
+  }
+  res.redirect("/");
+}
 
 
 
 module.exports = router;
+
